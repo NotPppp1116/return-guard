4547717f2754d047c89293f3d1480c337ace8d80
NotPppp1116 <usp9133@gmail.com>
2026-06-08 16:25:46 +0100

    Skip nested lambda bodies when collecting returns

--- src/internal/ReturnCollector.cpp
+++ src/internal/ReturnCollector.cpp
@@ -1,23 +1,21 @@
 #include "ReturnCollector.hpp"
 
-#include <clang/AST/ExprCXX.h>
 #include <clang/AST/Stmt.h>
 
 namespace returnguard::internal {
 
 ReturnCollector::ReturnCollector(
-    const clang::ASTContext& context,
     std::vector<const clang::Expr*>& expressions)
-    : context_(context), expressions_(expressions) {}
+    : expressions_(expressions) {}
+
+bool ReturnCollector::shouldVisitLambdaBody() const {
+    return false;
+}
 
 bool ReturnCollector::VisitReturnStmt(clang::ReturnStmt* statement) {
     if (statement->getRetValue() != nullptr) {
         expressions_.push_back(statement->getRetValue());
     }
-    return true;
-}
-
-bool ReturnCollector::TraverseLambdaExpr(clang::LambdaExpr*) {
     return true;
 }
 
