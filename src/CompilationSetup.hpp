662e0c87479c10cbe18ff3df441367b4ec144dc0
NotPppp1116 <usp9133@gmail.com>
2026-06-10 12:55:56 +0100

    Add automatic compilation setup helpers

---
+++ src/CompilationSetup.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <clang/Tooling/ArgumentsAdjusters.h>
+#include <llvm/ADT/ArrayRef.h>
+
+#include <optional>
+#include <string>
+
+namespace returnguard {
+
+std::optional<std::string> discover_compilation_database(
+    llvm::ArrayRef<const char*> arguments);
+
+clang::tooling::ArgumentsAdjuster resource_directory_adjuster();
+
+} // namespace returnguard
