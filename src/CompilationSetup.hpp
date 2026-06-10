87a3e17d754b400b59dd6b95ae49a71d685c9eef
NotPppp1116 <usp9133@gmail.com>
2026-06-10 18:39:09 +0100

    Expose compiler diagnostic adjusters

--- src/CompilationSetup.hpp
+++ src/CompilationSetup.hpp
@@ -12,5 +12,7 @@ std::optional<std::string> discover_compilation_database(
     llvm::ArrayRef<const char*> arguments);
 
 clang::tooling::ArgumentsAdjuster resource_directory_adjuster();
+clang::tooling::ArgumentsAdjuster compiler_diagnostic_adjuster();
+clang::tooling::ArgumentsAdjuster analyzer_argument_adjuster();
 
 } // namespace returnguard
