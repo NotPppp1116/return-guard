f8bc56e3938b4fb03511ee1700b58506dd490abf
NotPppp1116 <usp9133@gmail.com>
2026-06-10 22:12:44 +0100

    Connect instrumentation to analyzer state

--- src/internal/Analyzer.hpp
+++ src/internal/Analyzer.hpp
@@ -22,6 +22,7 @@ class Expr;
 class ForStmt;
 class FunctionDecl;
 class IfStmt;
+class Rewriter;
 class SourceManager;
 class Stmt;
 class SwitchStmt;
@@ -37,11 +38,12 @@ namespace returnguard::internal {
 
 class CFGValueFlow;
 class HandlerFinder;
+class Instrumentation;
 class NullStateAnalysis;
 
 class Analyzer final : public clang::RecursiveASTVisitor<Analyzer> {
   public:
-    explicit Analyzer(clang::ASTContext& context);
+    explicit Analyzer(clang::ASTContext& context, clang::Rewriter* rewriter = nullptr);
     ~Analyzer();
 
     [[nodiscard]] bool shouldVisitTemplateInstantiations() const;
@@ -53,26 +55,26 @@ class Analyzer final : public clang::RecursiveASTVisitor<Analyzer> {
     [[nodiscard]] const clang::SourceManager& source_manager() const;
 
     [[nodiscard]] CheckResult analyze_switch(const clang::SwitchStmt* statement,
-                                             const Domain& domain) const;
+                                              const Domain& domain) const;
     [[nodiscard]] CheckResult analyze_if_chain(const clang::IfStmt* statement,
-                                               const clang::VarDecl* variable,
-                                               const Domain& domain) const;
-    [[nodiscard]] CheckResult analyze_if_chain(const clang::IfStmt* statement,
-                                               const ExpressionSet& aliases,
-                                               const Domain& domain) const;
-    [[nodiscard]] CheckResult analyze_condition(const clang::Expr* condition,
                                                 const clang::VarDecl* variable,
                                                 const Domain& domain) const;
-    [[nodiscard]] CheckResult analyze_condition(const clang::Expr* condition,
+    [[nodiscard]] CheckResult analyze_if_chain(const clang::IfStmt* statement,
                                                 const ExpressionSet& aliases,
                                                 const Domain& domain) const;
+    [[nodiscard]] CheckResult analyze_condition(const clang::Expr* condition,
+                                                 const clang::VarDecl* variable,
+                                                 const Domain& domain) const;
+    [[nodiscard]] CheckResult analyze_condition(const clang::Expr* condition,
+                                                 const ExpressionSet& aliases,
+                                                 const Domain& domain) const;
 
     [[nodiscard]] std::optional<Domain>
     expression_domain(const clang::Expr* expression,
                       std::unordered_set<const clang::FunctionDecl*>& active_functions,
                       std::unordered_set<const clang::VarDecl*>& active_variables);
     [[nodiscard]] Domain function_domain(const clang::FunctionDecl* function,
-                                         std::unordered_set<const clang::FunctionDecl*>& active);
+                                          std::unordered_set<const clang::FunctionDecl*>& active);
     [[nodiscard]] Domain function_domain(const clang::FunctionDecl* function);
     [[nodiscard]] Domain call_domain(const clang::CallExpr* call);
     [[nodiscard]] bool call_returns_nullable_pointer(const clang::CallExpr* call) const;
@@ -110,7 +112,7 @@ class Analyzer final : public clang::RecursiveASTVisitor<Analyzer> {
                                    const clang::VarDecl* variable) const;
     [[nodiscard]] const clang::Expr*
     enclosing_assignment_conditional_condition(const clang::CallExpr* call,
-                                               const clang::VarDecl* variable) const;
+                                                const clang::VarDecl* variable) const;
     [[nodiscard]] bool call_is_forwarded(const clang::CallExpr* call) const;
     [[nodiscard]] bool call_is_discarded_expression(const clang::CallExpr* call) const;
     [[nodiscard]] bool call_is_operator(const clang::CallExpr* call) const;
@@ -134,14 +136,15 @@ class Analyzer final : public clang::RecursiveASTVisitor<Analyzer> {
     [[nodiscard]] std::string missing_message(const std::vector<DomainValue>& missing) const;
 
     [[nodiscard]] CheckResult analyze_variable(const clang::CallExpr* call,
-                                               const clang::VarDecl* variable,
-                                               const Domain& domain);
+                                                const clang::VarDecl* variable,
+                                                const Domain& domain);
     [[nodiscard]] CheckResult classify_call(const clang::CallExpr* call, const Domain& domain);
     [[nodiscard]] bool should_report(const CheckResult& result, const Domain& domain) const;
     void analyze_call(clang::CallExpr* call);
 
     clang::ASTContext& context_;
     clang::SourceManager& source_manager_;
+    std::unique_ptr<Instrumentation> instrumentation_;
     std::unordered_map<const clang::FunctionDecl*, Domain> domain_cache_;
     std::unordered_map<const clang::FunctionDecl*, std::unique_ptr<CFGValueFlow>> value_flow_cache_;
     std::unordered_set<const clang::FunctionDecl*> value_flow_failures_;
