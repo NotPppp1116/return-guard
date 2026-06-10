8472a7e5603d7bbc961481bcb25922faf890e4a4
NotPppp1116 <usp9133@gmail.com>
2026-06-10 22:16:47 +0100

    Add instrumentation regression input

---
+++ tests/cases/instrumentation.c
@@ -0,0 +1,21 @@
+#include <fcntl.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int consume_fd(int fd);
+
+int instrumentation_sample(const char* path, size_t size) {
+    void* memory = malloc(size);
+
+    int handled = open(path, O_RDONLY);
+    if (handled < 0) {
+        free(memory);
+        return -1;
+    }
+
+    int nested = consume_fd(open(path, O_RDONLY));
+    close(handled);
+    free(memory);
+    return nested;
+}
