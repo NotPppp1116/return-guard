60f261fb4457c1cc8f4c31804ffd6564a138a061
NotPppp1116 <usp9133@gmail.com>
2026-06-10 22:28:02 +0100

    Narrow LLVM formatter compatibility to APSInt

--- src/internal/APIntStringCompat.hpp
+++ src/internal/APIntStringCompat.hpp
@@ -1,17 +1,17 @@
 #pragma once
 
-#include <llvm/ADT/APInt.h>
-#include <llvm/ADT/SmallString.h>
+#include <llvm/ADT/APSInt.h>
+#include <llvm/ADT/StringExtras.h>
 #include <llvm/Config/llvm-config.h>
 
 #include <string>
 
 #if LLVM_VERSION_MAJOR < 19
 namespace llvm {
-inline std::string toString(const APInt& value, unsigned radix, bool signed_value) {
-    SmallString<64> buffer;
-    value.toString(buffer, radix, signed_value);
-    return std::string(buffer);
+
+inline std::string toString(const APSInt& value, unsigned radix, bool signed_value) {
+    return llvm::toString(static_cast<const APInt&>(value), radix, signed_value, false);
 }
+
 } // namespace llvm
 #endif
