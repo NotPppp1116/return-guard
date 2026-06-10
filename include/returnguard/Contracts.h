70cfbfa351c57d99c8af72b72bae0d9c81811f8f
NotPppp1116 <usp9133@gmail.com>
2026-06-10 22:55:42 +0100

    Make contract markers Clang-specific

--- include/returnguard/Contracts.h
+++ include/returnguard/Contracts.h
@@ -1,10 +1,8 @@
 #pragma once
 
-#if defined(__clang__) || defined(__GNUC__)
-#define RETURNGUARD_FAILS_NULL                                                   \
-    __attribute__((annotate("returnguard.failure:null")))
-#define RETURNGUARD_FAILS_NEGATIVE                                               \
-    __attribute__((annotate("returnguard.failure:negative")))
+#if defined(__clang__)
+#define RETURNGUARD_FAILS_NULL __attribute__((annotate("returnguard.failure:null")))
+#define RETURNGUARD_FAILS_NEGATIVE __attribute__((annotate("returnguard.failure:negative")))
 #else
 #define RETURNGUARD_FAILS_NULL
 #define RETURNGUARD_FAILS_NEGATIVE
