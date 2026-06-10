c520ae5b9b43c26d4860d8a38fa7c3ae69906fb4
NotPppp1116 <usp9133@gmail.com>
2026-06-10 18:46:45 +0100

    Implement compiler warning arguments

--- src/TestNative.cpp
+++ src/TestNative.cpp
@@ -1,1 +1,37 @@
-int returnguard_native_helper = 0;
+#include "CompilationSetup.hpp"
+
+#include <returnguard/Options.hpp>
+
+#include <array>
+#include <string>
+#include <string_view>
+
+namespace returnguard {
+
+clang::tooling::ArgumentsAdjuster compiler_diagnostic_adjuster() {
+    return [](const clang::tooling::CommandLineArguments& arguments, llvm::StringRef) {
+        clang::tooling::CommandLineArguments adjusted = arguments;
+        static constexpr std::array<std::string_view, 5> warnings = {
+            "array-bounds",
+            "return-stack-address",
+            "shift-count-overflow",
+            "shift-overflow",
+            "null-dereference",
+        };
+        for (const std::string_view warning : warnings) {
+            adjusted.push_back("-W" + std::string(warning));
+            if (options().fail_on_diagnostics) {
+                adjusted.push_back("-Werror=" + std::string(warning));
+            }
+        }
+        return adjusted;
+    };
+}
+
+clang::tooling::ArgumentsAdjuster analyzer_argument_adjuster() {
+    return [](const clang::tooling::CommandLineArguments& arguments, llvm::StringRef) {
+        return arguments;
+    };
+}
+
+} // namespace returnguard
