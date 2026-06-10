8cf630a5389789bfab2aa385506b76b02a3f71c2
NotPppp1116 <usp9133@gmail.com>
2026-06-10 18:50:11 +0100

    Add compiler warning configuration

--- src/Options.cpp
+++ src/Options.cpp
@@ -1,4 +1,10 @@
+#include "CompilationSetup.hpp"
+
 #include <returnguard/Options.hpp>
+
+#include <array>
+#include <string>
+#include <string_view>
 
 namespace returnguard {
 namespace {
@@ -11,6 +17,31 @@ void set_options(Options new_options) {
 
 const Options& options() {
     return global_options;
+}
+
+clang::tooling::ArgumentsAdjuster compiler_diagnostic_adjuster() {
+    return [](const clang::tooling::CommandLineArguments& arguments, llvm::StringRef) {
+        clang::tooling::CommandLineArguments adjusted = arguments;
+        static constexpr std::array<std::string_view, 4> warnings = {
+            "array-bounds",
+            "return-stack-address",
+            "shift-count-overflow",
+            "shift-overflow",
+        };
+        for (const std::string_view warning : warnings) {
+            adjusted.push_back("-W" + std::string(warning));
+            if (options().fail_on_diagnostics) {
+                adjusted.push_back("-Werror=" + std::string(warning));
+            }
+        }
+        return adjusted;
+    };
+}
+
+clang::tooling::ArgumentsAdjuster analyzer_argument_adjuster() {
+    return [](const clang::tooling::CommandLineArguments& arguments, llvm::StringRef) {
+        return arguments;
+    };
 }
 
 } // namespace returnguard
