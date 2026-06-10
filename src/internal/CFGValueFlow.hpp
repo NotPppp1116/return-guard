3e33c14b875e1756507032acddd3b1a6821e09de
NotPppp1116 <usp9133@gmail.com>
2026-06-10 13:28:35 +0100

    Add Clang CFG value-flow interface

---
+++ src/internal/CFGValueFlow.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "Model.hpp"
+
+#include <memory>
+
+namespace clang {
+class ASTContext;
+class CFG;
+class CallExpr;
+class FunctionDecl;
+} // namespace clang
+
+namespace returnguard::internal {
+
+class CFGValueFlow final {
+  public:
+    static std::unique_ptr<CFGValueFlow> build(
+        const clang::FunctionDecl& function,
+        clang::ASTContext& context);
+
+    [[nodiscard]] ExpressionSet aliases_for(const clang::CallExpr& call) const;
+
+  private:
+    CFGValueFlow(
+        std::unique_ptr<clang::CFG> cfg,
+        clang::ASTContext& context);
+
+    std::unique_ptr<clang::CFG> cfg_;
+    clang::ASTContext& context_;
+};
+
+} // namespace returnguard::internal
